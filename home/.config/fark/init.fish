97f9787a5b15cef710f2dc099c44664c99167d92
Jacob Farkas <jacobf@rkas.net>
2016-05-12 00:59:07 -0700

    Breaking out a file for local configuration

--- home/.config/fark/init.fish
+++ home/.config/fark/init.fish
@@ -1,5 +1,8 @@
 if set -q FRK_PATH
     source $FRK_PATH/config.fish
     source $FRK_PATH/aliases.fish
+    if test -f $FRK_PATH/localconfig.fish
+        source $FRK_PATH/localconfig.fish
+    end
     source $FRK_PATH/workconfig.fish
 end
