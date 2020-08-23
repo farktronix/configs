10b2d81da4ec8e307b1e5093c6e2727683d0cd33
Jacob Farkas <jacobf@rkas.net>
2020-08-23 08:56:44 -0700

    fixing python junk

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,8 +8,3 @@ for file in $HOME/.config/fish/conf.d/*.fish
 end
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-
-set -x PATH "$HOME/.pyenv/bin" $PATH
-set -x PATH "$HOME/gs-venv/bin" $PATH
-status --is-interactive; and . (pyenv init -|psub)
-status --is-interactive; and . (pyenv virtualenv-init -|psub)
